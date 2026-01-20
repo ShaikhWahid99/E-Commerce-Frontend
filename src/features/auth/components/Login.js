16b884a10ddd3bb47723f5e5e2ac2241125ebd4d
ShaikhWahid99 <shaikhabdulwahid19@gmail.con>
2026-01-20 16:22:50 +0530

    Added login as admin button

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -11,9 +11,9 @@ export default function Login() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm();
-
 
   return (
     <>
@@ -35,7 +35,7 @@ export default function Login() {
             noValidate
             onSubmit={handleSubmit((data) => {
               dispatch(
-                loginUserAsync({ email: data.email, password: data.password })
+                loginUserAsync({ email: data.email, password: data.password }),
               );
             })}
             className="space-y-6"
@@ -50,11 +50,11 @@ export default function Login() {
               <div className="mt-2">
                 <input
                   id="email"
-                  {...register('email', {
-                    required: 'email is required',
+                  {...register("email", {
+                    required: "email is required",
                     pattern: {
                       value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,
-                      message: 'email not valid',
+                      message: "email not valid",
                     },
                   })}
                   type="email"
@@ -86,8 +86,8 @@ export default function Login() {
               <div className="mt-2">
                 <input
                   id="password"
-                  {...register('password', {
-                    required: 'password is required',
+                  {...register("password", {
+                    required: "password is required",
                   })}
                   type="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -96,7 +96,9 @@ export default function Login() {
                   <p className="text-red-500">{errors.password.message}</p>
                 )}
               </div>
-              {error && <p className="text-red-500">{error || error.message}</p>}
+              {error && (
+                <p className="text-red-500">{error || error.message}</p>
+              )}
             </div>
 
             <div>
@@ -107,10 +109,23 @@ export default function Login() {
                 Log in
               </button>
             </div>
+
+            <div className="flex justify-center">
+              <button
+                type="button"
+                onClick={() => {
+                  setValue("email", "admin@gmail.com");
+                  setValue("password", "Qwerty@123");
+                }}
+                className="font-semibold leading-6  text-indigo-600 hover:text-indigo-500"
+              >
+                Login as Admin
+              </button>
+            </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
+            Not a member?{" "}
             <Link
               to="/signup"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
